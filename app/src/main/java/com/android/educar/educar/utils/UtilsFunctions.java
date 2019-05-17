e665e7624623617bffb805aba3cff15816b3f3b6
Kassio Holanda <holandakassio@hotmail.com>
2019-05-17 09:14:34 -0300

    adiconando metodo titlecase

--- app/src/main/java/com/android/educar/educar/utils/UtilsFunctions.java
+++ app/src/main/java/com/android/educar/educar/utils/UtilsFunctions.java
@@ -106,5 +106,18 @@ public class UtilsFunctions {
         return s2;
 
     }
+
+    public static String toTitledCase(String str) {
+
+        String[] words = str.split("\\s");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase());
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
 }
 
